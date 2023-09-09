4ca1b875a958c5259948d99bd486298109269c15
Silvers <logan.jma@gmail.com>
2023-09-08 22:12:00 -0500

    another paypal problem solved, this is not medium difficulty lol. More like hard

---
+++ medium/binary-search/maximum-value-at-a-given-index-in-a-bounded-array.js
@@ -0,0 +1,22 @@
+function maxValue(n, index, maxSum) {
+  // code here
+  let answer = 1;
+  let sum = n;
+  let pointerA = index;
+  let pointerB = index;
+
+  while(sum < maxSum) {
+    if(pointerB - pointerA === n - 1) {
+      answer += Math.floor((maxSum - sum) / n);
+      break;
+    }
+    sum += pointerB - pointerA + 1;
+    if(sum <= maxSum) answer++;
+    if(pointerB < n - 1) pointerB++;
+    if(pointerA > 0) pointerA--;
+  }
+  return answer;
+}
+// [2, 3, 2, 1]
+
+console.log('max value', maxValue(4, 2, 6));
