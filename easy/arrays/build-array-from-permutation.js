642773d6d5bb9607044767deeeb3d2b85a82f4f4
Ray Silvers <logan.jma@gmail.com>
2024-03-07 23:04:18 -0600

    added another problem

---
+++ easy/arrays/build-array-from-permutation.js
@@ -0,0 +1,13 @@
+const n = [0, 2, 1, 5, 3, 4];
+
+function buildArray(nums) {
+  // code here
+  const ans = [];
+  for(let i = 0; i < nums.length; i++) {
+    let temp = nums[nums[i]];
+    ans.push(temp);
+  }
+  return ans;
+};
+
+console.log('build array', buildArray(n));
