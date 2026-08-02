5a540383caa37e2ec666f6cc63b44a6d145ddf51
Ray Silvers <logan.jma@gmail.com>
2026-08-01 22:46:13 -0500

    another problem solved and practiced tonight. The basic valid parentheses.

--- rerun.js
+++ rerun.js
@@ -1,26 +1,25 @@
-const maxSlidingWindow = (nums, k) => {
-  const dq = [];
-  const res = [];
+const isValid = (s) => {
+  const pairs = { '}': '{', ']': '[', ')': '(' };
+  const stack = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    if (dq.length && dq[0] <= i - k) {
-      dq.shift();
-    }
-
-    while (dq.length && nums[dq[dq.length - 1]] <= nums[i]) {
-      dq.pop();
-    }
-
-    dq.push(i);
-
-    if (i >= k - 1) {
-      res.push(nums[dq[0]]);
+  for (const c of s) {
+    if (c === '(' || c === '{' || c === '[') {
+      stack.push(c);
+    } else {
+      if (stack.length === 0 || stack.pop() !== pairs[c]) {
+        return false;
+      }
     }
   }
-  return res;
+
+  return stack.length === 0;
 };
 
-console.log('[1,3,-1,-3,5,3,6,7], k=3 ->', maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3)); // [3,3,5,5,6,7]
-console.log('[1], k=1                 ->', maxSlidingWindow([1], 1));        // [1]
-console.log('[9,11], k=2              ->', maxSlidingWindow([9, 11], 2));    // [11]
-console.log('[4,-2], k=2              ->', maxSlidingWindow([4, -2], 2));    // [4]
+console.log('"()"      ->', isValid('()'));       // true
+console.log('"()[]{}"  ->', isValid('()[]{}'));   // true
+console.log('"(]"      ->', isValid('(]'));       // false
+console.log('"([)]"    ->', isValid('([)]'));     // false
+console.log('"([{}])"  ->', isValid('([{}])'));   // true
+console.log('"("       ->', isValid('('));        // false
+console.log('")"       ->', isValid(')'));        // false
+
