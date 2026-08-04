a1d53338d95a7e3030e07db8c67593ff6ae11846
Ray Silvers <logan.jma@gmail.com>
2026-08-03 21:55:10 -0500

    finsihed another stack question. Daily Temperatures.

--- scratch.js
+++ scratch.js
@@ -1,25 +1,20 @@
-const isValid = (s) => {
-  const pairs = { '}': '{', ']': '[', ')': '(' };
+const dailyTemperatures = (temperatures) => {
+  const n = temperatures.length;
+  const answer = new Array(n).fill(0);
   const stack = [];
 
-  for (const c of s) {
-    if (c === '(' || c === '{' || c === '[') {
-      stack.push(c);
-    } else {
-      if (stack.length === 0 || stack.pop() !== pairs[c]) {
-        return false;
-      }
+  for (let i = 0; i < n; i++) {
+    while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+      const j = stack.pop();
+      answer[j] = i - j;
     }
+    stack.push(i);
   }
 
-  return stack.length === 0;
+  return answer;
 };
 
-console.log('"()"      ->', isValid('()'));       // true
-console.log('"()[]{}"  ->', isValid('()[]{}'));   // true
-console.log('"(]"      ->', isValid('(]'));       // false
-console.log('"([)]"    ->', isValid('([)]'));     // false
-console.log('"([{}])"  ->', isValid('([{}])'));   // true
-console.log('"("       ->', isValid('('));        // false
-console.log('")"       ->', isValid(')'));        // false
-
+console.log('[73,74,75,71,69,72,76,73] ->', dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])); // [1,1,4,2,1,1,0,0]
+console.log('[30,40,50,60]             ->', dailyTemperatures([30, 40, 50, 60]));                   // [1,1,1,0]
+console.log('[30,60,90]                ->', dailyTemperatures([30, 60, 90]));                        // [1,1,0]
+console.log('[90,80,70]                ->', dailyTemperatures([90, 80, 70]));                        // [0,0,0]
